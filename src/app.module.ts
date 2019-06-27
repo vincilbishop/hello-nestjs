443c12e407000c5908bd2a4707669b1c5431588a
vsurge <vbishop@surgeforward.com>
2019-06-27 12:13:16 -0500

    Updated for services and seeds (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WidgetModule } from './widget/widget.module';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), WidgetModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
