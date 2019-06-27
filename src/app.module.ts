99b7d2c075ea16e9a24f20a2a384b752e722712e
vsurge <vbishop@surgeforward.com>
2019-06-27 12:20:33 -0500

    Adde nest-bull queue and controllers/authentication (#3)
    * Updated for services and seeds
    
    * Adde nest-bull queue and controllers/authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WidgetModule } from './widget/widget.module';
 import { Connection } from 'typeorm';
+import { ControllersModule } from './controllers/controllers.module';
+import { AuthenticationService } from './authentication/authentication.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), WidgetModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    BullModule.forRoot({
+      name: 'store',
+      options: {
+        redis: {
+          port: 6379,
+        },
+      },
+      processors: [
+        (job: Job, done: DoneCallback) => { done(null, job.data); },
+      ],
+    }),
+    WidgetModule,
+    ControllersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthenticationService],
 })
 export class AppModule {
   constructor(private readonly connection: Connection) {}
